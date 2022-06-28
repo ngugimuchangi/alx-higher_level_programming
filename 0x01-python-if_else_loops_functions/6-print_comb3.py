e7e5a3abe82818c25b27cbc51b029f1901360572
ngugimuchangi <tech.curious14@gmail.com>
2022-06-28 22:25:31 +0000

    Task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(1, 10):
+        if (j < i) | (j == i):
+            continue
+        if (j == 1) & (i == 0):
+            print('{:d}{:d}'.format(i, j), end='')
+        else:
+            print(', {:d}{:d}'.format(i, j), end='')
+print(''.format());
