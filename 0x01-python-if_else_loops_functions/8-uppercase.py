df66e8f8147416c7c4f620271dd47d99420ab212
ngugimuchangi <tech.curious14@gmail.com>
2022-06-28 17:09:23 +0000

    Task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/urs/bin/python3
+def uppercase(str):
+    s2 = ''
+    for i in str:
+        if ord(i) in range(97, 123):
+            s2 = s2 + chr(ord(i) - 32)
+        else:
+            s2 = s2 + i
+    print('{}'.format(s2))
