e0ecb0514bebc3981317d956d458728f2380128b
ngugimuchangi <tech.curious14@gmail.com>
2022-07-02 16:47:08 +0000

    check for null string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.strip('cC')
-    return new_string
+    if my_string:
+        new_string = my_string.strip('cC')
+        return new_string
