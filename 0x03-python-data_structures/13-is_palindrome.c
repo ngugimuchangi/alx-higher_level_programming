b8368beb1b4568712fa2ae50404c015d1ba85545
ngugimuchangi <tech.curious14@gmail.com>
2022-07-03 03:42:13 +0000

    task 13 fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,7 +7,7 @@
  */
 int is_palindrome(listint_t **head)
 {
-	int len, i;
+	int len, len_cpy, i, j;
 	listint_t *temp, *temp2;
 
 	temp = *head;
@@ -22,12 +22,14 @@ int is_palindrome(listint_t **head)
 	else
 		for (i = 0; i < (len / 2 + 1); i++, temp2 = temp2->next)
 			;
-	temp = *head;
+	len_cpy = len - 1;
 	for (i = 0; i < (len / 2) - 1; i++)
 	{
+		for (j = 0, temp = *head; j < len_cpy; j++)
+			temp = temp->next;
 		if (temp->n != temp2->n)
 			return (0);
-		temp = temp->next;
+		len_cpy--;
 		temp2 = temp2->next;
 	}
 	return (1);
