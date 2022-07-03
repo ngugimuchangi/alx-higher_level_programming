7270597119eb0c8b048761db5ad201a117bf336b
ngugimuchangi <tech.curious14@gmail.com>
2022-07-03 00:48:32 +0000

    value assignment fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = 0
-    b = 0
     if (len(tuple_a) >= 2) & (len(tuple_b) >= 2):
         a = tuple_a[0] + tuple_b[0]
         b = tuple_a[1] + tuple_b[1]
@@ -17,5 +15,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif (len(tuple_a) < 2) & (len(tuple_b) >= 2):
         a = tuple_a[0] + tuple_b[0]
         b = tuple_b[1]
+    else:
+        a = 0
+        b = 0
     tuple_d = (a, b)
     return tuple_d
