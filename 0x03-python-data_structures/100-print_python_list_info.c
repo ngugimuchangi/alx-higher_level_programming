d7f7eea755468777cdcf9f9661625ca89ba7b254
ngugimuchangi <tech.curious14@gmail.com>
2022-07-04 09:12:49 +0000

    task 14 fix

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,9 +1,8 @@
-#include <object.h>
-#include <listobject.h>
 #include <stdio.h>
+#include <Python.h>
 /**
  * print_python_list_info - prints info on Python lists
- * @p: pointer to list object
+ * @p: pointer to python object
  *
  * Description: prints size of list, allocated fields and
  * the elements within list and their types.
@@ -18,8 +17,8 @@ void print_python_list_info(PyObject *p)
 	if (PyList_Check(temp))
 	{
 		printf("[*] Size of the Python List = %li\n", PyList_Size(temp));
-		printf("[*] Allocated = %li\n", PyList_Size(temp))
+		printf("[*] Allocated = %li\n", temp->allocated)
 		for (i = 0; i < PyList_Size(temp); i++)
-			printf("Element %li: %s\n", i, PY_TYPE(PyList_GetItem(temp, i));
+			printf("Element %li: %s\n", i, PY_TYPE(PyList_GetItem(temp, i))->tp_name);
 	}
 }
