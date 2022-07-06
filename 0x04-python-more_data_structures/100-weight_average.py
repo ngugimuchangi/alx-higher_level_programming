d5fa263f642e7fa6b7600a684b22a96475acf565
ngugimuchangi <tech.curious14@gmail.com>
2022-07-06 23:56:09 +0000

    pycodestyle fix

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -7,5 +7,5 @@ def weight_average(my_list=[]):
         for tup in my_list:
             weighted_scores.append(tup[0] * tup[1])
             weights.append(tup[1])
-        avg = float(sum (weighted_scores) / sum(weights))
+        avg = float(sum(weighted_scores) / sum(weights))
     return avg
