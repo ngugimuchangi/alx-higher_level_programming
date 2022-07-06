25f0923f08de443a897ccb8e387118be05a12229
ngugimuchangi <tech.curious14@gmail.com>
2022-07-06 23:54:20 +0000

    task 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    avg = 0
+    if my_list:
+        weighted_scores = []
+        weights = []
+        for tup in my_list:
+            weighted_scores.append(tup[0] * tup[1])
+            weights.append(tup[1])
+        avg = float(sum (weighted_scores) / sum(weights))
+    return avg
