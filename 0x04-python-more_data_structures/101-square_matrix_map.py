d0f4cfa1c40006f86c34c5aed29737c578a426f9
ngugimuchangi <tech.curious14@gmail.com>
2022-07-07 00:19:39 +0000

    3 lines requirement fix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    if matrix:
-        new = [list(map(lambda x: x ** 2, [x for x in row])) for row in matrix]
-        return new
+    return [list(map(lambda x: x ** 2, [x for x in row])) for row in matrix]
