2706c26eb67b7279aa4f1ee4cf7f4d6a03d31048
ngugimuchangi <tech.curious14@gmail.com>
2022-07-18 13:41:56 +0000

    removed extra value assignment

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,17 +2,15 @@
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
+        res = 0
         try:
             res = my_list_1[i] / my_list_2[i]
         except IndexError:
             print("out of range".format())
-            res = 0
         except TypeError:
             print("wrong type".format())
-            res = 0
         except ZeroDivisionError:
             print("division by zero".format())
-            res = 0
         finally:
             new_list.append(res)
     return new_list
