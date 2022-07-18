ee53b732473fe67614617766ff9c37f299c912b1
ngugimuchangi <tech.curious14@gmail.com>
2022-07-18 14:14:40 +0000

    check for type error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,5 +4,5 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as error:
+    except (ValueError, TypeError) as error:
         sys.stderr.write("Exception: {}\n".format(error))
