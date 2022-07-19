56fc7c2aa7af32410ab628bd72dff26643293b87
ngugimuchangi <tech.curious14@gmail.com>
2022-07-19 18:52:52 +0000

    Pycodestyle documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,6 +3,14 @@
 
 
 class Square:
-    '''Initialize data'''
+    ''' Represents a square
+    Attributes:
+        __size(int): size of a square's die
+    '''
     def __init__(self, size):
+        '''Initialize instance of sqaure
+        Args:
+            size (int): size of a square's side
+        Return: None
+        '''
         self.__size = size
