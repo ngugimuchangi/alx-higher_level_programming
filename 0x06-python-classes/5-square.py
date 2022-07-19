187f5f6596a7aa62c2ab58aa9794a59f115e8f58
ngugimuchangi <tech.curious14@gmail.com>
2022-07-19 22:25:58 +0000

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+'''Square module'''
+
+
+class Square:
+    ''' Represents a square
+    Attributes:
+        __size(int): size of a square's die
+    '''
+    def __init__(self, size=0):
+        ''' Initialize instance of sqaure '''
+        self.__size = size
+
+    @property
+    def size(self):
+        ''' Retrieves value of size '''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''Updates value of size'''
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        ''' Calculates the area of the square '''
+        area = self.size * self.size
+        return area
+
+    def my_print(self):
+        if (self.__size > 0):
+            for i in range(self.__size):
+                for i in range(self.__size):
+                    print(f"#", end="")
+                print()
+        else:
+            print()
