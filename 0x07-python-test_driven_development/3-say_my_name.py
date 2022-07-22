89201f30aa3da991002d985df18760a255a028b9
ngugimuchangi <tech.curious14@gmail.com>
2022-07-22 18:54:53 +0000

    Task 3

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Printing module
+    Functions:
+        say_my_name(first_name, last_name)
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+        Function that prints names
+        Args:
+            first_name: first name of person
+            last_name: last name of person
+        Return: nothing
+    """
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+
+    if type(last_name) is not str:
+        raise TypeError('last_name must be a string')
+
+    print("{:s} {:s}".format(first_name, last_name))
