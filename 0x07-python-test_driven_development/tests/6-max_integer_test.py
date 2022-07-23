cf74031ac4b60f9d54e261fa8e4fc368d4f47702
ngugimuchangi <tech.curious14@gmail.com>
2022-07-23 12:55:33 +0000

    Task 5

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+    Unit test module for max_integer function
+    Class:
+        TestMaxInteger(unittest.TestCase)
+        Methods:
+            test_max_integer
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    def test_max_interger(self):
+        """
+            Test method for right output
+            Test cases: no list, ascending, descending,
+                            random, more than one max value
+        """
+        self.assertAlmostEqual(max_integer([]), None)
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([4, 3, 2, 1]), 4)
+        self.assertAlmostEqual(max_integer([1, 5, 0, 5]), 5)
+        self.assertAlmostEqual(max_integer([1, 9, 3, 7]), 9)
+
+    def test_max_integer_raises(self):
+        """
+            Test method for errors raised
+            Test cases: non list data types, list with none integer data types,
+                        list with mixed data types
+        """
+        self.assertRaises(TypeError, max_integer("hello"))
+        self.assertRaises(TypeError, max_integer((1, 2)))
+        self.assertRaises(TypeError, max_integer(["hello", "hi"]))
+        with self.assertRaises(TypeError):
+            max_integer([1, "hello"])
+        with self.assertRaises(TypeError):
+            max_integer(1)
