36395a7c5b04018f4f0dbfe8e191afa6c6790951
ngugimuchangi <tech.curious14@gmail.com>
2022-07-25 22:52:04 +0000

    Unknown error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,8 +13,8 @@ class Rectangle:
         """
             Object instace initialization method
         """
-
-        self.__width, self.__height = width, height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
@@ -30,7 +30,7 @@ class Rectangle:
         """
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -48,6 +48,6 @@ class Rectangle:
         """
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
