76a73ef3dfb6fa20b3e7a8ada518dd8b98226be9
ngugimuchangi <tech.curious14@gmail.com>
2022-07-25 22:57:32 +0000

    Task 1 fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,8 +13,8 @@ class Rectangle:
         """
             Object instace initialization method
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
