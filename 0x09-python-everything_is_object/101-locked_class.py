badf6d1a3473d2aff64cae545819b2a31e413d90
ngugimuchangi <tech.curious14@gmail.com>
2022-07-26 23:37:50 +0000

    Prevented __dict__ creation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,17 +6,6 @@
 
 class LockedClass:
     """
-        LockedClass class
-        Attributes: first_name, __setattr__
+        LockedClass
     """
-    first_name = ""
-
-    def __setattr__(self, key, value):
-        """
-            Dunder method for limiting the addition of new attributes
-        """
-        if hasattr(self, key):
-            self.__dict__[key] = value
-        else:
-            raise AttributeError(f"'LockedClass' object has no attribute" +
-                                 f" '{key}'")
+    __slots__ = ['first_name']
