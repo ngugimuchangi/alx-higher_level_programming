af29aad9ed32d83fe16e3b6682e804db1973ace6
ngugimuchangi <tech.curious14@gmail.com>
2022-07-26 00:18:45 +0000

    Task 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -78,7 +78,7 @@ class Rectangle:
         if self.__width > 0 and self.__height > 0:
             for i in range(self.__height):
                 for j in range(self.__width):
-                    rec_str += str(Rectangle.print_symbol)
+                    rec_str += str(self.print_symbol)
                 if i != self.__height - 1:
                     rec_str += "\n"
         return rec_str
