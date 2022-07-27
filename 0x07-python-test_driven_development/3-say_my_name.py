295489cbda99dfe9432fb0e65bf72753c7ffd8bd
ngugimuchangi <tech.curious14@gmail.com>
2022-07-27 22:05:34 +0000

    Fixed printing prefix

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -20,4 +20,4 @@ def say_my_name(first_name, last_name=""):
     if type(last_name) is not str:
         raise TypeError('last_name must be a string')
 
-    print(first_name, last_name)
+    print("My name is", first_name, last_name)
