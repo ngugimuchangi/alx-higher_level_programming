4289f6a83eb013d00193f9f91d633d2a9b124c84
ngugimuchangi <tech.curious14@gmail.com>
2022-07-27 23:02:16 +0000

    Unittests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 """
-    Unit test module for max_integer function
-    Class:
-        TestMaxInteger(unittest.TestCase)
-        Methods:
-            test_max_integer
+    Unit test module for max_integer function from 6-max_integer module
 """
 import unittest
 max_integer = __import__('6-max_integer').max_integer
@@ -13,13 +9,21 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """
         TestCase subclass
-        Use: testing max_integer function in 6-max_integer module
     """
+
+    def test_module_docstring(self):
+        """ Test module's docstring """
+        mod = __import__('6-max_integer').__doc__
+        self.assertTrue(len(mod) > 1)
+
+    def test_function_docstring(self):
+        """ Test function's docstring """
+        self.assertTrue(len(max_integer.__doc__) > 1)
 
     def test_max_interger(self):
         """
-            Test method for right output
-            Test cases: no list, ascending, descending,
+            Test for normal case scenarios
+            Test cases: empty list, ascending, descending,
                             random, more than one max value
         """
         self.assertAlmostEqual(max_integer([]), None)
@@ -27,11 +31,10 @@ class TestMaxInteger(unittest.TestCase):
         self.assertAlmostEqual(max_integer([4, 3, 2, 1]), 4)
         self.assertAlmostEqual(max_integer([1, 5, 0, 5]), 5)
         self.assertAlmostEqual(max_integer([1, 9, 3, 7]), 9)
-        self.assertTrue(len(max_integer) > 1)
 
     def test_max_integer_raises(self):
         """
-            Test method for errors raised
+            Test for error scnarios
             Test cases: non list data types, list with none integer data types,
                         list with mixed data types
         """
