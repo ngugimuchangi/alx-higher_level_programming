02f3a18ba7abacba25f430cea6c6279a7a3e8903
ngugimuchangi <tech.curious14@gmail.com>
2022-07-27 02:34:05 +0000

    Lazy matrix and Cpython strings

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,19 @@
+"""
+    Lazy matrix module
+    Functions:
+        lazy_matrix_mul(m_a, m_b)
+"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+        Lazy matrix function that multiplies two matrices using
+        numpy module
+        Args:
+            m_a: first matrix
+            m_b: second matrix
+        Return: matrix with multiplication result
+    """
+
+    return numpy.matmul(m_a, m_b)
