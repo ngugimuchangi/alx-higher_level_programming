3c43927acd888246fd3a85d3ba8024110c4d33f8
ngugimuchangi <tech.curious14@gmail.com>
2022-07-31 20:07:45 +0000

    fix: check for float overflow and value error for NaN types

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -19,7 +19,4 @@ def add_integer(a, b=98):
         raise TypeError('a must be an integer')
     if type(b) not in [int, float]:
         raise TypeError('b must be an integer')
-    res = a + b
-    if res == float('inf') or res == -float('inf'):
-        return 89
     return int(a) + int(b)
