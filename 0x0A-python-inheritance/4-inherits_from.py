737a14b2f840ea173e7965f0e6c2da33ac0c6cf9
ngugimuchangi <tech.curious14@gmail.com>
2022-08-01 21:23:18 +0000

    task 4: check inheritance from superclass and not current class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Check for inheritance of a class
+"""
+
+
+def inherits_from(obj, a_class):
+    """ Function to check if object is an instance
+        of a class that inherited (directly or indirectly)
+        from specified class
+
+        Args:
+            obj: object to check
+            a_class: superclass to check
+        Return: True if subclass, False if not
+    """
+    if hasattr(obj, '__class__') and a_class != "" \
+            and hasattr(a_class, '__class__') and \
+            obj.__class__ != a_class:
+        return issubclass(obj.__class__, a_class)
