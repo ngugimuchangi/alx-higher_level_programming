d06be598dacf7ef1162cfdf3ce47f2658314e1a3
ngugimuchangi <tech.curious14@gmail.com>
2022-08-01 20:58:55 +0000

    task 3: function to checks full path of object's inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/urs/bin/python3
+""" Check base classes
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ Function to check if class is specific superclass
+        Args:
+            obj: object to check
+            a_class: class to check
+    """
+    if a_class != "" and hasattr(a_class, '__class__'):
+        return isinstance(obj, a_class)
