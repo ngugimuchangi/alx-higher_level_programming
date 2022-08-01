fa73f90674af26c6ec53969b9e48adb29c117fab
ngugimuchangi <tech.curious14@gmail.com>
2022-08-01 20:36:42 +0000

    task 2: function to check if object is of specific instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Check instance module
+"""
+
+
+def is_same_class(obj, a_class):
+    """ Function to check if object is of a particular class
+        Args:
+            obj: name of object
+            a_class: class name to check for
+    """
+    if a_class != "":
+        return isinstance(obj, a_class)
