4c172b72746c969706bb2c81c24264788ffc18ea
ngugimuchangi <tech.curious14@gmail.com>
2022-08-02 19:38:05 +0000

    task 7: json serialization and deserialization

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" JSON serialization and deserialization module
+"""
+import sys
+import json
+dump = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
+
+
+def main():
+    """ Main function
+    """
+    try:
+        my_list = load('add_item.json')
+    except Exception:
+        dump([], 'add_item.json')
+    else:
+        my_list += [sys.argv[i] for i in range(1, len(sys.argv))]
+        dump(my_list, 'add_item.json')
+
+
+main()
