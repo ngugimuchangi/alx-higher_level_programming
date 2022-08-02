f38e8dd64e95b790a78bbe046a31b1384af13dd5
ngugimuchangi <tech.curious14@gmail.com>
2022-08-02 22:15:33 +0000

    task 11: added function that replaces all attributes of student instance

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" JSON class serialization module
+"""
+
+
+class Student:
+    """ Student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation method
+            Args:
+                first_name(str): student's first name
+                last_name(str): student's surname
+                age(int): student's age
+            Return: nothing
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ JSON representation of class
+            Args: none
+            Return: dictionary with requested key/value
+                    pairs
+        """
+        if attrs is None:
+            return self.__dict__
+        my_dict = {i: self.__dict__.get(i) for i in attrs
+                   if i in self.__dict__.keys()}
+        return my_dict
+
+    def reload_from_json(self, json):
+        """ Method that replaces all attributes of Student instance
+            Args:
+                json: new dictionary
+            Return: nothing
+        """
+        for i in json.keys():
+            self.__dict__[i] = json.get(i)
