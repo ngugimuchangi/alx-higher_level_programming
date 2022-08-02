787b8153985a01a6b221284568c730863112cd13
ngugimuchangi <tech.curious14@gmail.com>
2022-08-02 01:01:35 +0000

    task 11: added string representation of square object

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Square module
+"""
+base_class = __import__('9-rectangle').Rectangle
+
+
+class Square(base_class):
+    """ Square class: subclass of Rectanlge class
+    """
+
+    def __init__(self, size):
+        """ Instantiation method
+            Args:
+                size(int): size of square
+            Return: nothing
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ Area method to compute Square object's area
+            Args: none
+            Return: area of Square object
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """ __str__ methods
+            Args: none
+            Return: unofficial, formatted string representation of
+            Square object
+        """
+        return f"[Square] {self.__size}/{self.__size}"
