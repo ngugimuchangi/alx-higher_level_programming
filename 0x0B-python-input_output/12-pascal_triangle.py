889bde1749e2e2fcde956f16540f55d693e1f9df
ngugimuchangi <tech.curious14@gmail.com>
2022-08-02 23:03:07 +0000

    task 12: pascal triangle function

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/urs/bin/python3
+""" Technical interview module
+"""
+
+
+def pascal_triangle(n):
+    """ Pascal triangle function
+        Args:
+            n: number of row of binomial coefficients
+        Return: list of lists of binomial coefficients (ints)
+    """
+    if n <= 0:
+        return []
+    p_tri = [[1]]
+    for i in range(n - 1):
+        new_row = []
+        for x in range(len(p_tri[i])):
+            if x == 0:
+                new_row.append(p_tri[i][x])
+                try:
+                    new_row.append(p_tri[i][x] + p_tri[i][x + 1])
+                except Exception:
+                    new_row.append(p_tri[i][x])
+            else:
+                try:
+                    new_row.append(p_tri[i][x] + p_tri[i][x + 1])
+                except Exception:
+                    new_row.append(p_tri[i][x])
+        p_tri.append(new_row)
+    return p_tri
