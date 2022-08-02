c4d31b2e5cf24c8dc5744b209272a4fc7c71f236
ngugimuchangi <tech.curious14@gmail.com>
2022-08-02 00:24:56 +0000

    task 10: rectangle subclass 'square'

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Square module
+"""
+base_class = __import__('9-rectangle').Rectangle
+
+
+class Square(base_class):
+    """ Square class: subclass of Rectanlge class
+    """
+
+    def __init__(self, size):
+        base_class.__init__(self, size, size)
