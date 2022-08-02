24e1839944f8f7c3e9a811694dda5cebcc2636ac
ngugimuchangi <tech.curious14@gmail.com>
2022-08-02 17:12:43 +0000

    task 0: function to read files and print content

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3
+""" Read file module
+"""
+
+
+def read_file(filename=""):
+    """ Read file function
+        Args:
+            filename (str): name of file
+        Return: nothing
+    """
+    with open(filename, 'r', encoding='UTF8') as f:
+        print(f.read(), end="")
