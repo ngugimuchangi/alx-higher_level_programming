04acd3359c0ec95ddd16d50e42fd676a7f855ab6
ngugimuchangi <tech.curious14@gmail.com>
2022-08-03 15:33:47 +0000

    fix: reset status tally to zero after printing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -18,6 +18,7 @@ def status_print(status_tally, file_size):
     for key in sorted(status_tally.keys()):
         if status_tally[key]:
             print("{:s}: {:d}".format(key, status_tally[key]))
+            status_tally[key] = 0
 
 
 if __name__ == "__main__":
