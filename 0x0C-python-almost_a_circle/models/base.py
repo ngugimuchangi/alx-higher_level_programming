33cbfd100a99efaf139ba23b77451d029af1c7ef
ngugimuchangi <tech.curious14@gmail.com>
2022-08-05 15:55:47 +0000

    task: create new instance from json files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,7 +32,7 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ Static method to convert dictionaries into json representation
+        """ Method to convert dictionaries into json representation
             Args:
                 list_dictionaries (list): a list of dictionaries
             Return: nothing
@@ -45,25 +45,61 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """ Class method that coverts instance dictionaray to json object
+        """ Method that coverts instance dictionaray to json object
             and saves it to a file
             Args:
                 list_objs (Base): base class instance
             Return: nothing
         """
         if type(list_objs) is None:
-            with open(f"None.json", 'w', encoding='UTF8') as f:
-                json.dumps([], f)
+            with open(f"{cls.__name__}.json", 'w', encoding='UTF8') as f:
+                f.write(cls.to_json_string([]))
         if type(list_objs) is list:
-            for i in list_objs:
-                if isinstance(i, cls):
-                    my_dict = [i.to_dictionary()]
-                    with open(f"{i.__class__.__name__}.json",
-                              'a+', encoding='UTF8') as f:
-                        f.seek(0)
-                        line = f.read()
-                    with open(f"{i.__class__.__name__}.json",
-                              'w', encoding='UTF8') as f:
-                        if line:
-                            my_dict = json.loads(line) + my_dict
-                        f.write(cls.to_json_string(my_dict))
+            my_dict = [i.to_dictionary() for i in list_objs
+                       if isinstance(i, cls)]
+            with open(f"{cls.__name__}.json", 'w', encoding='UTF8') as f:
+                f.write(cls.to_json_string(my_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ Method that returns the list of the JSON string representation
+            Args:
+                json_string(str): string representing a list of dictionary
+            Return: object respresented as json string
+        """
+        if type(json_string) is None or json_string == "":
+            return []
+        if type(json_string) is str:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ Class constructor method
+            Args:
+                dictionary (kwargs): dictionary with a list of
+                                     keyword arguments
+            Return: new class
+        """
+        new = cls(1, 2)
+        new.update(**dictionary)
+        return new
+
+    @classmethod
+    def load_from_file(cls):
+        """ Method to to read json file
+            Args: none
+            Return: nothing
+        """
+        try:
+            f = open(f"{cls.__name__}.json", 'r', encoding='UTF8')
+        except FileNotFoundError:
+            return []
+        else:
+            line = f.read()
+            f.close
+            if line != "":
+                my_list = cls.from_json_string(line)
+                list_instances = []
+                for i in my_list:
+                    list_instances.append(cls.create(**i))
+                return list_instances
