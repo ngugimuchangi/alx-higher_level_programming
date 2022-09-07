8504418be86dca0dce9a1f0a2f2a9f6d0c17bbd1
ngugimuchangi <tech.curious14@gmail.com>
2022-09-07 03:17:32 +0000

    task: advanced task 18

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,4 @@
+-- Script that displays the average temperature by city
+-- sorted by temperature in descending order
+
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
