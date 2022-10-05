9090e38b25750358196b5f00c32bf2cf7f97afae
ngugimuchangi <tech.curious14@gmail.com>
2022-10-05 04:25:06 +0000

    fix: task 9 function requirement

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,12 +2,18 @@
 
 // Script to add two integer argument
 
+// Addition function
+function add (a, b) {
+  const res = a + b;
+  return res;
+}
+
 const args = process.argv.splice(2);
 const len = args.length;
 
 if (len < 2 || isNaN(args[0]) || isNaN(args[1])) {
   console.log('NaN');
 } else {
-  let sum = parseInt(args[0]) + parseInt(args[1]);
+  const sum = add(parseInt(args[0]), parseInt(args[1]));
   console.log(sum);
 }
