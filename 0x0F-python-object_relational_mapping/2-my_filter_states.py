19413d0ad4d6378d9b8789b6d1b03c3fad5d3757
ngugimuchangi <tech.curious14@gmail.com>
2022-10-08 08:23:26 +0000

    pycode style fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host='localhost', port=3306, user=user_name,
                            passwd=password, db=db_name, charset="utf8")
     cur = conn.cursor()
-    query = "SELECT * FROM states WHERE BINARY name='{}' ORDER BY id ASC".format(search)
-    cur.execute(query)
+    query = "SELECT * FROM states WHERE BINARY name='{}'ORDER BY id ASC"
+    cur.execute(query.format(search))
     data = cur.fetchall()
     for row in data:
         print(row)
