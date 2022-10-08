3cb00db798e85cfc006ed071808e00b48ce5dfe5
ngugimuchangi <tech.curious14@gmail.com>
2022-10-08 07:08:10 +0000

    include port number

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,9 +7,12 @@ import sys
 
 if __name__ == "__main__":
     user_name, password, db_name = sys.argv[1:]
-    db = MySQLdb.connect('localhost', user_name, password, db_name)
-    cur = db.cursor()
-    states = cur.execute("SELECT * FROM states ORDER BY id ASC")
-    print(states)
+    conn = MySQLdb.connect(host='localhost', port=3306,
+                           user=user_name, passwd=password, db=db_name)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    data = cur.fetchall()
+    for row in data:
+        print(data)
     cur.close()
-    db.close()
+    conn.close()
