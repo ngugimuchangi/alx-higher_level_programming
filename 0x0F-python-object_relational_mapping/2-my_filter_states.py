d181829fdd29991b446c873e67d2210e40dd2cd5
ngugimuchangi <tech.curious14@gmail.com>
2022-10-08 08:19:11 +0000

    fixed query string

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,9 +12,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host='localhost', port=3306, user=user_name,
                            passwd=password, db=db_name, charset="utf8")
     cur = conn.cursor()
-    query = "SELECT * FROM states WHERE name='{:s}' ORDER BY id ASC".format(
-            search)
-    cur.execute(query)
+    cur.execute("SELECT * FROM states WHERE BINARY name='{:s}'" +
+                "ORDER BY id ASC".format(search))
     data = cur.fetchall()
     for row in data:
         print(row)
