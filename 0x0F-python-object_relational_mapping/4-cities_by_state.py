936bf4679c7f74085c3932069fc2286f95181e94
ngugimuchangi <tech.curious14@gmail.com>
2022-10-08 09:16:16 +0000

    query string fix

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     query = "SELECT cities.*, states.name FROM cities {} {}".format(
             "LEFT OUTER JOIN states ON cities.state_id=states.id",
             "ORDER BY cities.id ASC")
-    cur.execute(query.format(search))
+    cur.execute(query)
     data = cur.fetchall()
     for row in data:
         print(row)
