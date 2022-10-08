026dec86b5837c25af31aea42183c7694f9326da
ngugimuchangi <tech.curious14@gmail.com>
2022-10-08 09:10:32 +0000

    task 4: searching across tables

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Script that lists all cities from the database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    user_name, password, db_name = sys.argv[1:]
+    conn = MySQLdb.connect(host='localhost', port=3306, user=user_name,
+                           passwd=password, db=db_name, charset="utf8")
+    cur = conn.cursor()
+    query = "SELECT cities.*, states.name FROM cities {} {}".format(
+            "LEFT OUTER states ON cities.state_id=states.id",
+            "ORDER BY cities.id ASC")
+    cur.execute(query.format(search))
+    data = cur.fetchall()
+    for row in data:
+        print(row)
+    cur.close()
+    conn.close()
