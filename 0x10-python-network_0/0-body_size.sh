55f2ee4802a330e62c6381f090480fb365b61f95
ngugimuchangi <tech.curious14@gmail.com>
2022-10-23 11:20:09 +0000

    empty task files

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Script send a request to a URL and displays the size of the body of the response
+
+if [ $# -gt 0 ]; then
+	curl -sI "$1" | grep  'Content-Length' | cut -d ':' -f 2
+fi
