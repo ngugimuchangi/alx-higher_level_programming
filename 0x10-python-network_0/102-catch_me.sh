44f49efa7600f742bac9b034906a4f25dfc877b3
ngugimuchangi <tech.curious14@gmail.com>
2022-10-23 18:49:15 +0000

    comment edit

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script to send a PUT request with data to append to a file
+# Script to send a PUT request and cause server to respond with a the message 'You got me!'
 curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
