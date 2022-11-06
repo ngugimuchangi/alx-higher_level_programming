adcdb6d6e2631fd73301779e50f7fda02a37bccd
ngugimuchangi <tech.curious14@gmail.com>
2022-11-06 11:18:15 +0000

    request lib: get header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" Script to HTTP request header
+"""
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    req = get(url)
+    print(req.headers.get('X-Request-Id'))
