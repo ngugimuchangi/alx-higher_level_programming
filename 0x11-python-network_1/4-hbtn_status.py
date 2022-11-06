0f24a4e170a13e68568907ae4642e94e38f25b4e
ngugimuchangi <tech.curious14@gmail.com>
2022-11-06 10:49:56 +0000

    request library

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" Script to send GET request using request library
+"""
+from requests import get
+
+
+if __name__ == "__main__":
+    req = get('https://alx-intranet.hbtn.io/status')
+    content = req.content.decode()
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
