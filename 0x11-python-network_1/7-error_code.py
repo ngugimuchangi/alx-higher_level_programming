7f17b8571ec29463fa38ca781aab15595ec08265
ngugimuchangi <tech.curious14@gmail.com>
2022-11-06 11:31:07 +0000

    get status

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Script that manages HTTP errors
+"""
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    req = get(url)
+    if req.status_code < 400:
+        print(req.content.decode())
+    else:
+        print("Error code: {}".format(req.status_code))
