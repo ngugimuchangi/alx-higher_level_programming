742f3abca7a6ab0066f3804eb2877e044b26d7f4
ngugimuchangi <tech.curious14@gmail.com>
2022-11-06 17:57:59 +0000

    github tasks

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" Script to login into github
+    Usage: ./10-my_github.py <username> <token>
+"""
+from requests import get
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    username, token = argv[1:3]
+    logins = HTTPBasicAuth(username, token)
+    req = get(url, auth=logins)
+    results = req.json()
+    print(results.get('id'))
--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+""" Script that gets last 10 commits
+
+"""
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/search/commits"
+    repo, owner = argv[1:3]
+    header = {"accept": "application/vnd.github+json"}
+    q = "repo:{}/{}".format(owner, repo)
+    data = {"q": q, "per_page": 100, "sort": "author-date"}
+    req = get(url, params=data headers=header)
+    if req.status_code == 200:
+        results = req.json()['items']
+        for commit in results:
+            print("{}: {}".format(commit.get('sha'), commit.get('author')))
