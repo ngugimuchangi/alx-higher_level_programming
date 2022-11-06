33722435dd9651d5f64b04c9bfbfb1c1ae746af3
ngugimuchangi <tech.curious14@gmail.com>
2022-11-06 11:26:00 +0000

    request lib: post

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" Script to send a POST request
+"""
+from requests import post
+from sys import argv
+
+
+if __name__ == "__main__":
+    url, email = argv[1:3]
+    data = {'email': email}
+    req = post(url, data=data)
+    print(req.content)
