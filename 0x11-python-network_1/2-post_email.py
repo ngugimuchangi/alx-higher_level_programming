98b0c15739baad1eaeece9ca2f0588ba46dac374
ngugimuchangi <tech.curious14@gmail.com>
2022-11-06 10:22:02 +0000

    fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,5 +8,5 @@ from urllib.parse import urlencode
 if __name__ == "__main__":
     url, email = argv[1:3]
     data = urlencode({'email': email}).encode('utf8')
-    with urlopen(Request(url, data)) as req:
-        print("Your email is: {}".format(req.msg))
+    with urlopen(url, data) as req:
+        print("Your email is: {}".format(req.read().decode('utf8')))
