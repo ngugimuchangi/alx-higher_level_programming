13886b19d0a709ff35652b1aa28c171d11988b07
ngugimuchangi <tech.curious14@gmail.com>
2022-11-06 10:32:02 +0000

    error handling

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" Script to send get request and manage HTTPError
+"""
+from sys import argv
+from urllib.request import urlopen
+from urllib.error import HTTPError
+
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        with urlopen(url) as req:
+            print(req.read().decode('utf-8'))
+    except HTTPError as error:
+        print("Error code: {}".format(error.code))
+
