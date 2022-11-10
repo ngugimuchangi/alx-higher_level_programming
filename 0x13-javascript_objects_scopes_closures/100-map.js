711f707af52563e7288efb4d66b86bc9c7d5fe13
ngugimuchangi <tech.curious14@gmail.com>
2022-11-10 05:19:16 +0000

    fixed map arrow function

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -8,5 +8,5 @@
 const list = require('./100-data').list;
 
 console.log(list);
-const newList = list.map(element => element * list.indexOf(element));
+const newList = list.map((element, index) => element * index);
 console.log(newList);
