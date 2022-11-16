78e3fc3590797ece279b598f3f34d3b35b98fba8
ngugimuchangi <tech.curious14@gmail.com>
2022-11-16 00:34:06 +0000

    simplified approach

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -10,30 +10,14 @@ request(url, (error, response, body) => {
   if (response) {
     const toDos = JSON.parse(body);
     const output = {};
-    let count = 0;
-    let userId = toDos[0].userId;
+
     for (const toDo of toDos) {
-      if (toDo.userId === userId) {
-        count++;
-      }
-      if (toDo.userId !== userId || toDos.indexOf(toDo) === toDos.length - 1) {
-        output[userId] = count;
-        count = 1;
-        userId = toDo.userId;
-      }
-    }
-
-    const keys = Object.keys(output);
-
-    for (const userId of keys) {
-      if (keys.indexOf(userId) === 0) {
-        console.log(`{ '${userId}': ${output[userId]},`);
-      } else if (keys.indexOf(userId) === keys.length - 1) {
-        console.log(`  '${userId}': ${output[userId]} }`);
+      if (output[toDo.userId] === undefined) {
+        output[toDo.userId] = 1;
       } else {
-        console.log(`  '${userId}': ${output[userId]},`);
+        output[toDo.userId]++;
       }
     }
-    //    console.log(output);
+    console.log(output);
   }
 });
